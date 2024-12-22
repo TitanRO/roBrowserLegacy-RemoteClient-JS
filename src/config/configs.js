a301e5929909c0481457278507f0a3ebd308280b
trojal <trojal@gmail.com>
2024-12-22 06:18:11 +0000

    Update configs.js

--- src/config/configs.js
+++ src/config/configs.js
@@ -1,7 +1,7 @@
 module.exports = {
 	DEBUG: true,
 	CLIENT_RESPATH: "resources/",
-	CLIENT_DATAINI: "DATA.INI",
+	CLIENT_DATAINI: "TDATA.INI",
 	CLIENT_AUTOEXTRACT: true,
 	CLIENT_ENABLESEARCH: true,
 };
